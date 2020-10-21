bc4a83b2ccadb457849d71582bc9c2309eff7549
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-21 11:36:01 +0100

    added header + logo

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Container, Col, Row} from 'react-bootstrap'
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container> 
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; City Centre Comics
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
