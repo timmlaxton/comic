1a7d84fa9149a6a805371d5960a071c90d41bf18
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-22 10:15:15 +0100

    created user model

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true
     })
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`)
