3cb4b0e2a3d7df7fd90bc382fcd61bdf60a1b73d
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-26 15:51:16 +0000

    can add + remove items from cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,7 +7,7 @@ import Loader from '../components/Loader'
 import {listProductDetails} from '../actions/productActions'
 
 const ProductScreen = ({match, history}) => {
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
 
   const dispatch = useDispatch()
 
