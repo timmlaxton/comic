b30cb30731570ea21fbaed1486eb70468eaf7c79
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-26 09:57:59 +0000

    added loader and message components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,7 @@ import Product from '../models/productModel.js'
 // Public
 router.get('/', asyncHandler (async (req, res) => {
    const products = await Product.find({})
+   
   res.json(products)
 }))
 
