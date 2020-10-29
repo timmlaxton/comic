4a0f9f2cf4fcf427dcd1773d8a4be4eb4cbec2c1
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-29 20:23:21 +0000

    addded update profile controller and route

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -91,7 +91,7 @@ const RegisterScreen = ({location, history,}) => {
 
       <Row className="py-3">
         <Col>
-        Have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Register</Link>
+        Have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
         </Col>
       </Row>
     </FormContainer>
