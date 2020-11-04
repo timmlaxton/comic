c834c8bcba3b62aafb7674fc6216b6c18ccfc315
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-11-04 15:11:01 +0000

    added checkout steps

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,7 +2,9 @@ import React, {useState} from 'react'
 import {Form, Button} from 'react-bootstrap';
 import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import {saveShippingAddress} from '../actions/cartActions'
+
 
 const ShippingScreen = ({history}) => {
   const cart = useSelector(state => state.cart)
@@ -23,6 +25,7 @@ const ShippingScreen = ({history}) => {
   }
   
   return <FormContainer>
+    <CheckoutSteps step1 step2/>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
       <Form.Group controlId='address'>
